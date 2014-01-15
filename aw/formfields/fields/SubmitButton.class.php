c662b9739bdf511a5c909c1725090284728433af
alexwyett <alex@carltonsoftware.co.uk>
2014-01-15 15:11:10 +0000

    New submit button field

---
+++ aw/formfields/fields/SubmitButton.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Submit button class
+ *
+ * PHP Version 5.3
+ *
+ * @category  FormFields
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2013 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+
+namespace aw\formfields\fields;
+
+/**
+ * Submit button object
+ *
+ * PHP Version 5.3
+ * 
+ * @category  FormFields
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2013 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+class SubmitButton extends \aw\formfields\fields\ValueElement
+{
+    /**
+     * Constructor
+     * 
+     * @param string $name       Field name
+     * @param array  $attributes Field attributes
+     * 
+     * @return aw\forms\fields\TextField
+     */
+    public function __construct($attributes = array())
+    {
+        // Add attributes
+        parent::__construct($attributes);
+        
+        // Set the template
+        $this->setTemplate(
+            '<input type="{getType}"{implodeAttributes}>'
+        );
+    }
+}
