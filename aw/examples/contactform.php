95fa30502c1dbceb530cfad460fcded612eca241
alexwyett <alex@carltonsoftware.co.uk>
2014-01-15 15:57:20 +0000

    New contact form example

---
+++ aw/examples/contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contact Form example
+ *
+ * PHP Version 5.3
+ *
+ * @category  FormFields
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2013 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+
+// Include autoloader
+require_once '../autoload.php';
+
+echo \aw\formfields\forms\ContactForm::factory(array(), $_GET);
