1fc6f1d26d1abc32e52717cefa03034aed7714e4
alexwyett <alex@carltonsoftware.co.uk>
2014-01-28 21:33:57 +0000

    Added src variable

--- aw/formfields/fields/ImageButton.class.php
+++ aw/formfields/fields/ImageButton.class.php
@@ -27,24 +27,63 @@ namespace aw\formfields\fields;
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link      http://www.github.com/alexwyett
  */
-class Image extends \aw\formfields\fields\ValueElement
+class ImageButton extends \aw\formfields\fields\ValueElement
 {
+    /**
+     * Image src
+     * 
+     * @var string
+     */
+    protected $src = '';
+    
     /**
      * Constructor
      * 
-     * @param string $name       Field name
+     * @param string $src        Image source
      * @param array  $attributes Field attributes
      * 
      * @return aw\forms\fields\TextField
      */
-    public function __construct($attributes = array())
+    public function __construct($src, $attributes = array())
     {
+        // Set the source
+        $this->setSrc($src);
+        
         // Add attributes
         parent::__construct($attributes);
         
         // Set the template
         $this->setTemplate(
-            '<input type="image" {implodeAttributes}>'
+            '<input type="image"{implodeAttributes}>'
         );
+    }
+    
+    // ------------------------- Accessor Methods -------------------------- //
+    
+    /**
+     * Return the image button source
+     * 
+     * @return string
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+    
+    /**
+     * Set the source
+     * 
+     * @param string $src Image source
+     * 
+     * @return aw\forms\fields\ImageButton
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+
+        // Set attribute value
+        $this->attributes['src'] = $this->getSrc();
+
+        return $this;
     }
 }
