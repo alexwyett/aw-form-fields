d77b7e9a43b9c58bce03eb030a7ac0afaaff8a55
alexwyett <alex@carltonsoftware.co.uk>
2014-01-31 14:14:34 +0000

    New Hidden field class

---
+++ aw/formfields/fields/HiddenInput.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Hidden input class
+ *
+ * PHP Version 5.3
+ *
+ * @category  FormFields
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2013 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+
+namespace aw\formfields\fields;
+
+/**
+ * Hidden input class.  Exactly the same functionality as TextInput except
+ * the getType method will return hidden.
+ *
+ * PHP Version 5.3
+ * 
+ * @category  FormFields
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2013 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+class HiddenInput extends \aw\formfields\fields\TextInput
+{
+    
+}
